8bb00840b8b0f661430769ae26d17abffca38daf
Rimma Maksiutova <nnearobot@gmail.com>
2023-01-18 00:07:23 +0900

    easy/746_MinCostClimbingStairs_top-bottom.java: import added

--- easy/746_MinCostClimbingStairs_top-bottom.java
+++ easy/746_MinCostClimbingStairs_top-bottom.java
@@ -5,6 +5,8 @@
  * You can either start from the step with index 0, or the step with index 1.
  * Return the minimum cost to reach the top of the floor.
  */
+import java.util.HashMap;
+
 class Solution {
     private HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
     private int[] cost;
