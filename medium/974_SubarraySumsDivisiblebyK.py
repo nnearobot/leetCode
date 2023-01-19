b189f8dfdbb3799aed5da278d91721d2f92cd363
Rimma Maksiutova <nnearobot@gmail.com>
2023-01-20 01:51:36 +0900

    medium/974_SubarraySumsDivisiblebyK.py

---
+++ medium/974_SubarraySumsDivisiblebyK.py
@@ -0,0 +1,29 @@
+"""
+974. Subarray Sums Divisible by K
+Medium
+https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+
+"""
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        prefixSumRemCount = {}
+        summ = 0
+        for i, n in enumerate(nums):
+            summ += n
+            rem = summ % k
+            if rem not in prefixSumRemCount:
+                prefixSumRemCount[rem] = 0
+            prefixSumRemCount[rem] += 1
+
+        res = 0
+        for rem in prefixSumRemCount:
+            count = prefixSumRemCount[rem]
+            if rem == 0:
+                res += count
+            if count == 2:
+                res += 1
+            if count > 2:
+                res = res + (count - 1) * count // 2
+
+        return res
