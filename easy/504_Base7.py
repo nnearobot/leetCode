83aca9bd225662cdb40af56c4d655dea9233aa5b
Rimma Maksiutova <nnearobot@gmail.com>
2023-02-02 00:42:28 +0900

    solution to 504. Base 7

--- easy/504_Base7.py
+++ easy/504_Base7.py
@@ -3,25 +3,14 @@
 # https://leetcode.com/problems/base-7
 
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        if len(nums) == 0:
-            return 0
-        if len(nums) == 1:
-            return 0 if nums[0] == val else 1
-
-        count = 0
-        end = len(nums)
-        for i, n in enumerate(nums):
-            if i == end:
-                break
-            while n == val:
-                end -= 1
-                if i == end:
-                    return count
-                tmp = nums[end]
-                if tmp != val:
-                    nums[end] = n
-                    nums[i] = tmp
-                n = tmp
-            count += 1
-        return count
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        res = ""
+        isNeg = num < 0
+        while num:
+            rem = abs(num) % 7
+            num = abs(num) // 7
+            res = str(rem) + res
+            print(res, num)
+        return "-" + res if isNeg else res
