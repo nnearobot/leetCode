59c20942c21d42832bfb21d9212b4d40455d3904
Rimma Maksiutova <nnearobot@gmail.com>
2023-02-01 23:49:38 +0900

    easy/1017_ConverttoBase-minus2.py

---
+++ easy/1017_ConverttoBase-minus2.py
@@ -0,0 +1,16 @@
+# 1017. Convert to Base -2
+# Easy
+# https://leetcode.com/problems/convert-to-base-2/description/
+
+class Solution:
+    def baseNeg2(self, n: int) -> str:
+        if n == 0:
+            return "0"
+        res = ""
+        while n:
+            rem = n % -2
+            n //= -2
+            if rem == -1:
+                n += 1
+            res = str(abs(rem)) + res
+        return res
