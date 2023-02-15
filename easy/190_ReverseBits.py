679d0c00899a85c3e88f2bf54b18630a0dae121e
Rimma Maksiutova <nnearobot@gmail.com>
2023-02-15 22:11:03 +0900

    190. Reverse Bits

---
+++ easy/190_ReverseBits.py
@@ -0,0 +1,7 @@
+# 190. Reverse Bits
+# Easy
+# https://leetcode.com/problems/reverse-bits
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        return int(bin(n)[2:].zfill(32)[::-1], 2)
