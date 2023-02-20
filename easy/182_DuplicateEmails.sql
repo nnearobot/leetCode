fd436608fb95f6d5041d6ad17fb1c99938434cfa
Rimma Maksiutova <nnearobot@gmail.com>
2023-02-20 23:10:03 +0900

    182. Duplicate Emails

---
+++ easy/182_DuplicateEmails.sql
@@ -0,0 +1,7 @@
+# 182. Duplicate Emails
+# Easy
+# https://leetcode.com/problems/duplicate-emails
+
+SELECT p2.email
+FROM (SELECT p1.email, COUNT(*) as count FROM Person p1 GROUP BY p1.email) as p2
+WHERE p2.count > 1
