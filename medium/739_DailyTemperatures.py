ec09119b4362e518bc12228c13c3530cf8927776
Rimma Maksiutova <nnearobot@gmail.com>
2023-06-03 16:26:39 +0900

    739. Daily Temperatures

---
+++ medium/739_DailyTemperatures.py
@@ -0,0 +1,14 @@
+# 739. Daily Temperatures
+# Medium
+# https://leetcode.com/problems/daily-temperatures
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                prev = stack.pop()
+                answer[prev] = i - prev
+            stack.append(i)
+        return answer
